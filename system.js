e2d19487c9840d960cf048624015a23324fab0ac
Eugene <roreugene@gmail.com>
2021-07-14 11:17:39 +0300

    System.js updated
    Added integrity checking; minor bug fixes

--- system.js
+++ system.js
@@ -1,36 +1,60 @@
-/* Preparing for output */
-const set = []
-
 /* Parsing */
-var fs = require('fs')
+var fs = require("fs")
 var prototype = JSON.parse(fs.readFileSync("/ax/prototype.json", "utf-8"))
 var data = JSON.parse(fs.readFileSync("/ax/data.json", "utf-8"))
+console.log("[________loaded_______]")
 
 /* Fetching prototypes */
-let get_length = (object) => {
-	return Object.keys(object).length
-}
-let get_prototype = (type) => {
-	if (type in prototype)
-	{
-		return prototype[type]
-	} else {
-		console.log("unhappy in get_prototype")
-	}
-}
+let get_length = (object) => { return Object.keys(object).length }
+let get_prototype = (type) => { return prototype[type] }
 
 /* Requests */
 let block_prototype = get_prototype("block")
 let event_prototype = get_prototype("event")
 
+/* Check integrity by prototype */
+let integrity = (data, block_prototype, event_prototype) => {
+	let flag = true
+	for (let key in data)
+	{
+		if ("id" in data[key] && !("name" in data[key])) {
+			for (let field in data[key])
+			{
+				if (!(field in block_prototype)) {
+					console.log("bad parameter in " + key + " block declaration: " + field)
+					flag = false
+				}
+			}
+		} else if ("name" in data[key] && !("id" in data[key])) {
+			for (let field in data[key])
+			{
+				if (!(field in event_prototype)) {
+					console.log("bad parameter in " + key + " event declaration: " + field)
+					flag = false
+				}
+			}
+		} else {
+			console.log("<!> unmatching prototype in integrity")
+			flag = false
+		}
+	}
+	return flag
+}
+if (integrity(data, block_prototype, event_prototype)) 
+{
+	console.log("[__integrity_checked__]")
+} else {
+	console.log("[____bad_integrity____]")
+	return
+}
+
 /* Enumerate data */
-let data_length = get_length(data)
 let data_headers = []
 let enumerate = 1
 for (let key in data)
 {
 	let constructor = {}
-	let current_key_length = get_length(key)
+	let current_key_length = get_length(data[key])
 	let current_priority = (current_key_length >= 3) ? 0 : 1
 	constructor.name = key
 	constructor.priority = current_priority
@@ -38,4 +62,25 @@ for (let key in data)
 	enumerate++
 	data_headers.push(constructor)
 }
-console.log(data_headers)
+console.log("[_headers_constructed_]")
+for (let i = 0; i < data_headers.length; i++)
+{
+	let locin = JSON.stringify(data_headers[i])
+	let concat = "\"" + i + "\"\:"
+	data_headers[i] = concat + locin
+}
+let concat_all = ""
+for (let i = 0; i < data_headers.length; i++)
+{
+	if (i === 0) concat_all += "{"
+	concat_all += data_headers[i]
+	if (i < data_headers.length-1) concat_all += ","
+	if (i === data_headers.length-1) concat_all += "}"
+}
+
+fs.writeFile("system_out.json", concat_all, function (err) {
+	if (err) return console.log(err)
+  console.log("[__system_out_success_]")
+	var leak = JSON.parse(fs.readFileSync("/ax/system_out.json", "utf-8"))
+	console.log(leak)
+})
