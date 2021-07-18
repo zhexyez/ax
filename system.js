28c3123b9d092093a8c93cb5f1261a626df20b08
Eugene <roreugene@gmail.com>
2021-07-18 14:16:43 +0300

    Review of System.js file
    Must be reviewed again ASAP

--- system.js
+++ system.js
@@ -40,39 +40,67 @@ let integrity = (data, block_prototype, event_prototype) => {
 				/* Check if more than 1 parameter presented */
 				console.log("<!> invalid length of object in " + key)
 				flag = false
-				return flag
 			}
 			if (!("type" in data[key])) {
 				/* Check if type presented in data[key] */
 				console.log("<!> no type presented in " + key)
 				flag = false
-				return flag
+			} else if (!(data[key]["type"] in block_types["block_types"])) {
+						/* Check if type presented in prototypes */
+						console.log("<!> bad type in " + key + " block declaration => " + data[key]["type"])
+						flag = false
+			} else if (block_types["block_types"][data[key]["type"]] == false) {
+						/* Check if type is avialable */
+						console.log("<!> type " + data[key][field] + " is unavialable")
+						flag = false
 			}
 			for (let field in data[key])
 			{
-				if (data[key][field].length == 0) {
-					/* Check if field is of 0 length */
-					console.log("<!> length must be at least 1 char in " + field + " in " + key)
-					flag = false
-					return flag
-				}
-				if (!(isNaN(data[key][field][0]))) {
-					/* Check if begins with char */
-					console.log("<!> must begin with char type in " + field + " in " + key)
-					flag = false
-					return flag
-				}
-				if (!(field in block_prototype)) {
-					/* Check if field presented in ptorotype */
-					console.log("<!> bad parameter in " + key + " block declaration: " + field)
-					flag = false
-					return flag
-				} else {
-					if (field == "type" && !(data[key][field] in block_types["block_types"])) {
-						/* Check if type presented in prototype */
-						console.log("<!> bad type in " + key + " block declaration: " + data[key][field])
+				if (field != "type") {
+					let type = data[key]["type"]
+					/* Check if field name is anything but type */
+					if (field.length == 0) {
+						/* Check if field name is of 0 length */
+						console.log("<!> length of field name must be at least 1 char")
 						flag = false
-						return flag
+					}
+					if (data[key][field].length == 0) {
+						/* Check if field is of 0 length */
+						console.log("<!> length must be at least 1 char in " + key + " => " + field)
+						flag = false
+					}
+					if (!(isNaN(data[key][field][0]))) {
+						/* Check if begins with char */
+						console.log("<!> must begin with char type in " + key + " => " + field)
+						flag = false
+					}
+					if (!(field in block_prototype) && !(field in block_types[type])) {
+						/* Check if prototypes matches */
+						console.log("<!> unmatching prototype in " + key + " => " + field)
+						flag = false
+					} else if (!(field in block_prototype) || !(field in block_types[type])) {
+						/* Check if value of field matches it's prototype value */
+						/* !!! REVIEW ASAP !!! */
+						let compare_types = (value_of_data_field, value_of_picked_field) => {
+							let compare_types_FLAG = false
+							for (let each in value_of_picked_field)
+							{
+								if (typeof(value_of_picked_field[each]) == typeof(value_of_data_field)) {
+									/* If types are equal */
+									compare_types_FLAG = true
+									return compare_types_FLAG
+								}
+							}
+							console.log("<!> unmatching value prototype in " + key + " => " + field)
+							return compare_types_FLAG
+						}
+						if (field in block_prototype) {
+							flag = compare_types(data[key][field], block_prototype[field])
+							if (flag == false) return flag
+						} else if (field in block_types[type]) {
+							flag = compare_types(data[key][field], block_types[type][field])
+							if (flag == false) return flag
+						}
 					}
 				}
 			}
@@ -81,9 +109,8 @@ let integrity = (data, block_prototype, event_prototype) => {
 			for (let field in data[key])
 			{
 				if (!(field in event_prototype)) {
-					console.log("<!> bad parameter in " + key + " event declaration: " + field)
+					console.log("<!> bad parameter in " + key + " event declaration => " + field)
 					flag = false
-					return flag
 				}
 			}
 		} else {
